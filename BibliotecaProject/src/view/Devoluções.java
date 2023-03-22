19092f38ad1a39e4917ca303c75af6a022289afd
aluno-16 <aluno-16@aluno16-OptiPlex-7010>
2023-03-22 14:51:49 -0300

    botoes de cadastrar e editar; botoes de apagar e adcionar.

--- BibliotecaProject/src/view/Devoluções.java
+++ BibliotecaProject/src/view/Devoluções.java
@@ -87,12 +87,9 @@ public class Devoluções extends javax.swing.JFrame {
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
             .addGroup(layout.createSequentialGroup()
                 .addContainerGap()
-                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-                    .addGroup(layout.createSequentialGroup()
-                        .addComponent(jVoltar)
-                        .addGap(0, 0, Short.MAX_VALUE))
-                    .addComponent(jScrollPane1, javax.swing.GroupLayout.DEFAULT_SIZE, 596, Short.MAX_VALUE))
-                .addContainerGap())
+                .addComponent(jVoltar)
+                .addContainerGap(530, Short.MAX_VALUE))
+            .addComponent(jScrollPane1, javax.swing.GroupLayout.DEFAULT_SIZE, 608, Short.MAX_VALUE)
         );
         layout.setVerticalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
